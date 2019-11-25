0db872b52b3598a170e58a97737bbc7c3a1e0e97
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-25 18:14:17 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   # This helper is created automatically with your Rails project and is where you keep helpers that are related to the application itself rather than any given model or controller.
 
   def title(text)
-  content_for :title, text
-end
+    content_for :title, text
+  end
 end
