62673a490330dad9c3e0bf461ee0aa9e81f3c095
Jamie Berrier <berrier.jamie@gmail.com>
2019-11-25 18:13:26 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  # This helper is created automatically with your Rails project and is where you keep helpers that are related to the application itself rather than any given model or controller.
 end
